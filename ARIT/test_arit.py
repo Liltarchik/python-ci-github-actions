dd0e8f33990bc00cfbfd72ce4e572a375a26f62d
Liltarchik <tarasikbilukha@gmail.com>
2025-10-23 11:58:31 +0300

    Add python files

---
+++ ARIT/test_arit.py
@@ -0,0 +1,20 @@
+from arit import add
+
+def test_add():
+    try:
+        assert add(2, 3) == 5
+        print("Addition works correctly.")
+    except AssertionError:
+        print("Addition failed.")    
+    try:
+        assert add(-1, 1) == 0
+        print("Addition works correctly.")    
+    except AssertionError:
+        print("Addition failed.")
+    try:
+        assert add(3, 3) == 8
+        print("Addition works correctly.")
+    except AssertionError:
+        print("Addition failed.")
+
+print(test_add())
